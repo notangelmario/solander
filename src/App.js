bc8f33d21a87128544b1a5faf64ae69cbc5e471b
clandestinz <clandestine8945135@gmail.com>
2020-07-09 23:32:28 +0300

    -Added ergaleio-library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-class App extends React.Component {
-  render(){
-    return (
-      <div className="App">  
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './ergaleio-library/css/ergaleio-main.css'
+import './ergaleio-library/css/ergaleio-sec.css'
+import './ergaleio-library/css/ergaleio-custom.css'
 
 ReactDOM.render(
   <React.StrictMode>

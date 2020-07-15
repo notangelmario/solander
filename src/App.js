7ca8f4df5415e3ab3a3dadeace88ed36f3ed0989
clandestinz <clandestine8945135@gmail.com>
2020-07-15 18:35:42 +0300

    Test github actions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import InfoPage from './components/pages/InfoPage'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+
+//Test Github Actions
 export default class App extends React.Component {
 
   state = {
